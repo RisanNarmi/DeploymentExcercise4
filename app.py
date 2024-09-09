09bfa7b7b5ca204cd12213fce86bdf7cae70f6a2
RisanNarmi <qwertzer8@gmail.com>
2024-09-09 20:29:59 +0800

    Update app.py
    Added slider

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentExcerci
 @callback(
     Output('graph-scatter', 'figure'),
     Output('graph-pie', 'figure'),
+    Output('graph-pie2', 'figure'),
     Input('dropdown-count', 'value'),
     Input('dropdown-year', 'value'),
+    Input('slider-year', 'value')
 )
-def update_graph(sel, yer):
+def update_graph(sel, yer, yerslide):
     subCountry = df[df["country"].isin([sel])]
     fig = px.scatter(subCountry, x="year", y="gdp")
 
@@ -32,7 +34,20 @@ def update_graph(sel, yer):
               "gdp":pie_data}
     fig2 = px.pie(pie_df, values="gdp", names="continent")
 
-    return fig, fig2
+    subYear = df[df["year"].isin([yerslide])]
+    subASIA_Year = subYear[subYear['state'].isin(['Asia'])]
+    subEU_Year = subYear[subYear['state'].isin(['Europe'])]
+    subOCE_Year = subYear[subYear['state'].isin(['Oceania'])]
+    subAMERICAS_Year = subYear[subYear['state'].isin(['America'])]
+    subAFRICA_Year = subYear[subYear['state'].isin(['Africa'])]
+    chart_Lable = ["Asia", "Europe", "Oceania", "Americas", "Africa"]
+    pie_data = sum(subASIA_Year["gdp"]), sum(subEU_Year["gdp"]), sum(subOCE_Year["gdp"]), sum(subAMERICAS_Year["gdp"]), sum(subAFRICA_Year["gdp"])
+    pie_df = {"continent":chart_Lable,
+              "gdp":pie_data}
+    fig3 = px.pie(pie_df, values="gdp", names="continent")
+    
+
+    return fig, fig2, fig3
 
 # layout set
 app.layout = [html.H1('Hello, look at this graph'), 
@@ -42,6 +57,8 @@ app.layout = [html.H1('Hello, look at this graph'),
               dcc.Dropdown([{'label':'2020', 'value':2020}, {'label':'2010', 'value':2010}, 
                             {'label':'2000', 'value':2000}], 2020, id='dropdown-year'),
               dcc.Graph(id="graph-pie")]
+              dcc.Slider(1960, 2020, 5, value=2020, id='slider-year'),
+              dcc.Graph(id="graph-pie2")]
 
 if __name__ == '__main__':
     app.run(debug=True)
