f6d12549782ad5740c8eacf6e728f6b9f95e3f36
RisanNarmi <qwertzer8@gmail.com>
2024-09-09 19:14:14 +0800

    Update app.py
    Added interactivity

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Input, Output # dcc is dash component
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import plotly.express as px
@@ -6,24 +6,41 @@ import plotly.express as px
 app = Dash(__name__)
 server = app.server
 
-df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentTestMCM7183Ex3/main/gdp_1960_2020.csv")
+# Reading data
+df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentExcercise4/main/gdp_1960_2020.csv")
 
-subMY = df[df["country"].isin(["Malaysia"])]
-sub2020 = df[df["year"].isin([2020])]
-subASIA_2020 = sub2020[sub2020['state'].isin(['Asia'])]
-subEU_2020 = sub2020[sub2020['state'].isin(['Europe'])]
-subOCE_2020 = sub2020[sub2020['state'].isin(['Oceania'])]
-subAMERICAS_2020 = sub2020[sub2020['state'].isin(['America'])]
-subAFRICA_2020 = sub2020[sub2020['state'].isin(['Africa'])]
+# setting figures
+fig = px.scatter(subCountry, x="year", y="gdp")
+fig2 = px.pie(pie_df, values="gdp", names="continent")
+
+# layout set
+app.layout = [html.H1('Hello, look at this graph'), 
+              html.H3('Interactivity time'), 
+              dcc.Dropdown(['Malaysia, Indonesia, China'], 'Malaysia', id='dropdown-count']), 
+              dcc.Graph(id='graph-scatter'), 
+              dcc.Graph(figure=fig2)]
+
+# Setting Paramaters
+subYear = df[df["year"].isin([2020])]
+subASIA_Year = sub2020[sub2020['state'].isin(['Asia'])]
+subEU_Year = sub2020[sub2020['state'].isin(['Europe'])]
+subOCE_Year = sub2020[sub2020['state'].isin(['Oceania'])]
+subAMERICAS_Year = sub2020[sub2020['state'].isin(['America'])]
+subAFRICA_Year = sub2020[sub2020['state'].isin(['Africa'])]
 chart_Lable = ["Asia", "Europe", "Oceania", "Americas", "Africa"]
-pie_data = sum(subASIA_2020["gdp"]), sum(subEU_2020["gdp"]), sum(subOCE_2020["gdp"]), sum(subAMERICAS_2020["gdp"]), sum(subAFRICA_2020["gdp"])
+pie_data = sum(subASIA_Year["gdp"]), sum(subEU_Year["gdp"]), sum(subOCE_Year["gdp"]), sum(subAMERICAS_Year["gdp"]), sum(subAFRICA_Year["gdp"])
 pie_df = {"continent":chart_Lable,
          "gdp":pie_data}
 
-fig = px.scatter(subMY, x="year", y="gdp")
-fig2 = px.pie(pie_df, values="gdp", names="continent")
+@callback(
+    Output('graph-scatter', 'figure'),
+    Input('dropdown-count', 'value')
+)
+def update_graph(sel):
+    subCountry = df[df["country"].isin(["sel"])]
+    fig = px.scatter(subset_Country, x="year", y="gdp")
+    return fig
 
-app.layout = [html.H1('Hello, look at this graph'), dcc.Graph(figure=fig), dcc.Graph(figure=fig2)]
 
 if __name__ == '__main__':
     app.run(debug=True)
